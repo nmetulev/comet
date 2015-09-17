f9a9e630a9209fa572aece57a59c1725a809c775
Nikola Metulev <nikolame@microsoft.com>
2015-09-17 16:32:30 -0700

    experimenting with nuget

--- Comet/Properties/AssemblyInfo.cs
+++ Comet/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CoMET")]
-[assembly: AssemblyDescription("Controls")]
+[assembly: AssemblyTitle("CoMet")]
+[assembly: AssemblyDescription("Controls by Metulev")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CoMET")]
+[assembly: AssemblyProduct("CoMet")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
