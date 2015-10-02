91ed1534f82790ac8e02cf86895b90bf0337e4c6
Nikola Metulev <nikolame@microsoft.com>
2015-10-01 22:47:33 -0700

    Added FindChildren method to Extensions

--- Comet/Extensions/XAMLExtensions.cs
+++ Comet/Extensions/XAMLExtensions.cs
@@ -9,24 +9,18 @@ using Windows.Graphics.Display;
 using Windows.Graphics.Imaging;
 using Windows.Storage.Streams;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
 
 namespace Comet.Extensions
 {
     public static class XAMLExtensions
     {
-        public static async Task<BitmapImage> RenderToBitmapImage(this UIElement element)
-        {
-
-           
-            using (var stream = await element.RenderToRandomAccessStream())
-            {
-                BitmapImage image = new BitmapImage();
-                image.SetSource(stream);
-                return image;
-            }
-        }
-
+        /// <summary>
+        /// Render a UIElement into a bitmap IRandomAccessStream
+        /// </summary>
+        /// <param name="element">The element to render</param>
+        /// <returns>An awaitable task that returns the IRandomAccessStream</returns>
         public static async Task<IRandomAccessStream> RenderToRandomAccessStream(this UIElement element)
         {
             if (element == null) throw new NullReferenceException();
@@ -54,6 +48,69 @@ namespace Comet.Extensions
 
             return stream;
         }
+
+        /// <summary>
+        /// Render a UIElement into a bitmap
+        /// </summary>
+        /// <param name="element">The element to render</param>
+        /// <returns>An awaitable task that returns the BitmapImage</returns>
+        public static async Task<BitmapImage> RenderToBitmapImage(this UIElement element)
+        {
+            using (var stream = await element.RenderToRandomAccessStream())
+            {
+                BitmapImage image = new BitmapImage();
+                image.SetSource(stream);
+                return image;
+            }
+        }
+
+        /// <summary>
+        /// Traverses the Visual Tree and returns a list of elements of type T
+        /// </summary>
+        /// <typeparam name="T">The type of elements to find</typeparam>
+        /// <param name="parent">The root of the Visual Tree</param>
+        /// <returns>A list of elements of type T, or null</returns>
+        public static IEnumerable<T> FindChildren<T>(this DependencyObject parent)
+        {
+            return FindChildren<T>(parent);
+        }
+
+        /// <summary>
+        /// A helper function for FindChildren
+        /// Traverses the Visual Tree and returns a list of elements of type T
+        /// </summary>
+        /// <typeparam name="T">The type of elements to find</typeparam>
+        /// <param name="parent">The root of the Visual Tree</param>
+        /// <param name="list">a list to be used in the recusive calls</param>
+        /// <returns>A list of elements of type T, or null</returns>
+        private static IEnumerable<T> _FindChildren<T>(DependencyObject parent, List<T> list = null)
+               where T : DependencyObject
+        {
+            // Confirm parent and childName are valid. 
+            if (parent == null) return null;
+
+            if (list == null) list = new List<T>();
+
+            int childrenCount = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < childrenCount; i++)
+            {
+                var child = VisualTreeHelper.GetChild(parent, i);
+                // If the child is not of the request child type child
+
+                T childType = child as T;
+                if (childType == null)
+                {
+                    _FindChildren<T>(child, list);
+                }
+                else
+                {
+                    list.Add(childType);
+                }
+            }
+
+            return list;
+        }
+
 
     }
 }
