fafbd3c3448351e6bee7c526135dfdea82cd0654
Nikola Metulev <nikolame@microsoft.com>
2015-10-01 23:14:22 -0700

    added converters

---
+++ Comet/Converters/DateTimeFormatConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace Comet.Converters
+{
+    /// <summary>
+    /// Converter to convert DateTime to string with the specified format (parameter)
+    /// </summary>
+    class DateTimeFormatConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            try
+            {
+                return System.Convert.ToDateTime(value).ToString(parameter.ToString());
+            }
+            catch { return value; }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Comet/Converters/HexToSolidColorBrushConverter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace Comet.Converters
+{
+    /// <summary>
+    /// Converts string conteining hex color of the form (#FFFFFFFF) to SolidColorBrush
+    /// </summary>
+    class HexToSolidColorBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            try
+            {
+                var hexCode = value as string;
+                var color = new Color();
+
+                if (hexCode.Length == 9)
+                {
+                    color.A = byte.Parse(hexCode.Substring(1, 2), NumberStyles.AllowHexSpecifier);
+                    color.R = byte.Parse(hexCode.Substring(3, 2), NumberStyles.AllowHexSpecifier);
+                    color.G = byte.Parse(hexCode.Substring(5, 2), NumberStyles.AllowHexSpecifier);
+                    color.B = byte.Parse(hexCode.Substring(7, 2), NumberStyles.AllowHexSpecifier);
+                }
+                else if (hexCode.Length == 7)
+                {
+                    color.A = 0xFF;
+                    color.R = byte.Parse(hexCode.Substring(1, 2), NumberStyles.AllowHexSpecifier);
+                    color.G = byte.Parse(hexCode.Substring(3, 2), NumberStyles.AllowHexSpecifier);
+                    color.B = byte.Parse(hexCode.Substring(5, 2), NumberStyles.AllowHexSpecifier);
+                }
+                else
+                {
+                    color.A = 0xFF;
+                    color.R = 0xFF;
+                    color.G = 0xFF;
+                    color.B = 0xFF;
+                }
+
+                return new SolidColorBrush(color);
+            }
+            catch { return value; }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
