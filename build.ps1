740f6654d0b5afc5c8b655c1827235f599b3d002
Thiago Almeida <nzthiago@live.com>
2015-10-05 18:26:09 +0000

    Updated build.ps1

--- build.ps1
+++ build.ps1
@@ -1,11 +1,5 @@
-# Import the Task.Common and Task.Internal dll that has all the cmdlets we need for Build
-import-module "Microsoft.TeamFoundation.DistributedTask.Task.Internal"
-import-module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
-    
-$nugetPath = Get-ToolPath -Name 'NuGet.exe'
-if (-not $nugetPath)
-{
-    Write-Warning (Get-LocalizedString -Key "Unable to locate nuget.exe. Package restore will not be performed for the solutions")
-}
+$sourceNugetExe = "https://dist.nuget.org/win-x86-commandline/v3.2.0/nuget.exe"
+$targetNugetExe = "nuget.exe"
+Invoke-WebRequest $sourceNugetExe -OutFile $targetNugetExe
 
-Invoke-Tool -Path $nugetPath -Arguments "restore Comet.sln -NonInteractive -Source https://api.nuget.org/v3/index.json"
+nuget.exe restore Comet.sln -NonInteractive 
