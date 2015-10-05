38c40b7819ed42907b2980736382ee850999731f
Thiago Almeida <nzthiago@live.com>
2015-10-05 17:42:46 +0000

    Updated build.ps1

--- build.ps1
+++ build.ps1
@@ -1,1 +1,7 @@
-NuGet.exe restore Comet.sln -NonInteractive -Source https://api.nuget.org/v3/index.json
+$nugetPath = Get-ToolPath -Name 'NuGet.exe'
+if (-not $nugetPath)
+{
+    Write-Warning (Get-LocalizedString -Key "Unable to locate nuget.exe. Package restore will not be performed for the solutions")
+}
+
+Invoke-Tool -Path $nugetPath -Arguments "restore Comet.sln -NonInteractive -Source https://api.nuget.org/v3/index.json"
