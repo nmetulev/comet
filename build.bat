fbefd9daf521a995abd171e363e95d7dc133653b
Nikola Metulev <nikolame@microsoft.com>
2015-10-05 22:15:02 -0700

    nuget and build script

---
+++ build.bat
@@ -0,0 +1,47 @@
+@echo off
+@echo.
+@echo *******************************************
+@echo * BUILD STARTING   			*
+@echo *******************************************
+@echo.
+
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsMSBuildCmd.bat"
+
+msbuild /verbosity:quiet /fl /t:Rebuild /p:Configuration=Release,OutputPath=bin\Release\NuGet\ /property:GenerateLibraryLayout=false /p:NoWarn=0618 Comet\Comet.csproj
+
+@echo.
+@echo *******************************************
+@echo * COPYING BINARIES                        *
+@echo *******************************************
+@echo.
+
+pushd Tools\nuget
+
+mkdir .\Comet\lib\uap10.0
+mkdir .\Comet\lib\uap10.0\Comet
+mkdir .\Comet\lib\uap10.0\Comet\Themes
+mkdir .\Comet\lib\uap10.0\Comet\Properties
+copy ..\..\Comet\bin\release\NuGet\Comet.dll .\Comet\lib\uap10.0\
+copy ..\..\Comet\bin\release\NuGet\Comet.pri .\Comet\lib\uap10.0\
+copy ..\..\Comet\bin\release\NuGet\Comet.xr.xml .\Comet\lib\uap10.0\Comet\
+copy ..\..\Comet\bin\release\NuGet\themes\generic.xbf .\Comet\lib\uap10.0\Comet\Themes
+copy ..\..\Comet\Properties\Comet.rd.xml .\Comet\lib\uap10.0\Comet\Properties
+
+@echo.
+@echo *******************************************
+@echo * BUILDING NUGET 				*
+@echo *******************************************
+@echo.
+
+mkdir .\package
+nuget pack Comet\Comet.nuspec -o .\package
+
+@echo.
+@echo *******************************************
+@echo * DONE 		 			*
+@echo *******************************************
+@echo.
+
+explorer .\package
+
+popd
