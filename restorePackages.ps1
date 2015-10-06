6d27c25fd4388eb109968ecbdc99557422c04503
Nikola Metulev <nikolame@microsoft.com>
2015-10-05 22:34:21 -0700

    fixed restorePackage bug

--- restorePackages.ps1
+++ restorePackages.ps1
@@ -1,5 +1,1 @@
-$sourceNugetExe = "https://dist.nuget.org/win-x86-commandline/v3.2.0/nuget.exe"
-$targetNugetExe = ".\nuget.exe"
-Invoke-WebRequest $sourceNugetExe -OutFile $targetNugetExe
-
-.\nuget.exe restore Comet.sln -NonInteractive 
+.Tools\nuget\nuget.exe restore Comet\build.sln -NonInteractive 
