36f267cdce2f62e5c220d7960ad9fac2d051de54
Nikola Metulev <nikolame@microsoft.com>
2015-11-19 22:33:15 -0800

    renamed file structure for easier navigation

--- build.bat
+++ build.bat
@@ -7,7 +7,7 @@
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsMSBuildCmd.bat"
 
-msbuild /verbosity:quiet /fl /t:Rebuild /p:Configuration=Release,OutputPath=bin\Release\NuGet\ /property:GenerateLibraryLayout=false /p:NoWarn=0618 Comet\Comet.csproj
+msbuild /verbosity:quiet /fl /t:Rebuild /p:Configuration=Release,OutputPath=bin\Release\NuGet\ /property:GenerateLibraryLayout=false /p:NoWarn=0618 src\Comet.csproj
 
 @echo.
 @echo *******************************************
@@ -21,11 +21,11 @@ mkdir .\Comet\lib\uap10.0
 mkdir .\Comet\lib\uap10.0\Comet
 mkdir .\Comet\lib\uap10.0\Comet\Themes
 mkdir .\Comet\lib\uap10.0\Comet\Properties
-copy ..\..\Comet\bin\release\NuGet\Comet.dll .\Comet\lib\uap10.0\
-copy ..\..\Comet\bin\release\NuGet\Comet.pri .\Comet\lib\uap10.0\
-copy ..\..\Comet\bin\release\NuGet\Comet.xr.xml .\Comet\lib\uap10.0\Comet\
-copy ..\..\Comet\bin\release\NuGet\themes\generic.xbf .\Comet\lib\uap10.0\Comet\Themes
-copy ..\..\Comet\Properties\Comet.rd.xml .\Comet\lib\uap10.0\Comet\Properties
+copy ..\..\src\bin\release\NuGet\Comet.dll .\Comet\lib\uap10.0\
+copy ..\..\src\bin\release\NuGet\Comet.pri .\Comet\lib\uap10.0\
+copy ..\..\src\bin\release\NuGet\Comet.xr.xml .\Comet\lib\uap10.0\Comet\
+copy ..\..\src\bin\release\NuGet\themes\generic.xbf .\Comet\lib\uap10.0\Comet\Themes
+copy ..\..\src\Properties\Comet.rd.xml .\Comet\lib\uap10.0\Comet\Properties
 
 @echo.
 @echo *******************************************
