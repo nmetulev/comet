60f2925a3898718426e3229b48df83bf19c12b39
Nikola Metulev <nikola.metulev@gmail.com>
2015-11-20 16:13:08 -0800

    Initial setup for SwipeableSplitView

---
+++ src/Controls/SwipeableSplitView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace Comet.Controls
+{
+    public class SwipeableSplitView : SplitView
+    {
+        public SwipeableSplitView()
+        {
+            DefaultStyleKey = typeof(SwipeableSplitView);
+        }
+    }
+}
