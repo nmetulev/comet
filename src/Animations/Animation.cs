9bb57ddb084f797bf5b5e09276af84b7cf83e0b2
Nikola Metulev <nikolame@microsoft.com>
2015-11-23 21:00:46 -0800

    started animation implementation

---
+++ src/Animations/Animation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Animation;
+
+namespace Comet.Animations
+{
+    public class Animation
+    {
+        private Storyboard _story { get; set; }
+
+        public Animation()
+        {
+            _story = new Storyboard();
+        }
+    }
+}
---
+++ src/Animations/AnimationHelpers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace Comet.Animations
+{
+    public static class AnimationHelpers
+    {
+        public static Animation Move( this UIElement element)
+        {
+            Animation animation = new Animation();
+            return animation;
+        }
+
+        public static Animation Move (this Animation animation)
+        {
+            return animation;
+        }
+
+        public static Animation Fade (this UIElement element)
+        {
+            Animation animation = new Animation();
+            return animation;
+        }
+
+        public static Animation Fade (this Animation animation)
+        {
+            return animation;
+        }
+    }
+}
