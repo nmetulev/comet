50212c058963705a9e4088736fa480436a90d762
Nikola Metulev <nikolame@microsoft.com>
2016-03-30 13:53:00 -0700

    updated project to target 10586 and update FlexiblePanel to use composition

--- src/Controls/FlexiblePanel/FlexiblePanel.cs
+++ src/Controls/FlexiblePanel/FlexiblePanel.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using Windows.Foundation;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Hosting;
 
 namespace Comet.Controls
 {
@@ -19,7 +21,11 @@ namespace Comet.Controls
         public static readonly DependencyProperty ItemDesiredHeightProperty =
             DependencyProperty.Register("ItemDesiredHeight", typeof(double), typeof(FlexiblePanel), new PropertyMetadata(150d));
 
+        bool firstTime = true;
 
+        public delegate void OrganizeCustomPanelLoadedEventHandler(object sender, EventArgs e);
+
+        public event OrganizeCustomPanelLoadedEventHandler OrganizeCustomPanelLoaded;
 
         protected override Size MeasureOverride(Size availableSize)
         {
@@ -30,51 +36,64 @@ namespace Comet.Controls
                 return resultSize;
             }
 
-            double y = 0;
-            double x = 0;
 
-            double maxX = 0;
+            double x = 0;
+            double y = 0;
+
             List<List<UIElement>> items = new List<List<UIElement>>();
 
             int row = 0;
 
-            foreach (var child in Children)
+            List<UIElement> currentRow = new List<UIElement>();
+
+            for (var i = 0; i < Children.Count; ++i)
             {
+                var curChild = Children[i];
+                curChild.Measure(new Size(availableSize.Width, ItemDesiredHeight));
 
-                child.Measure(new Size(availableSize.Width, ItemDesiredHeight));
-
-
-                if (x + child.DesiredSize.Width > availableSize.Width)
+                if (x + curChild.DesiredSize.Width > availableSize.Width)
                 {
-                    x = 0;
-                    y += child.DesiredSize.Height;
-                    row++;
+                    var widthDifWithChild = (x + curChild.DesiredSize.Width) - availableSize.Width;
+                    var widthDifWithoutChild = availableSize.Width - x;
+
+                    double scaleFactor = 1;
+                    if (widthDifWithChild < widthDifWithoutChild)
+                    {
+                        //increase size to all children including curCHild
+                        currentRow.Add(curChild);
+                        scaleFactor = availableSize.Width / (x + curChild.DesiredSize.Width);
+                        foreach (var item in currentRow)
+                        {
+                            item.Measure(new Size(availableSize.Width, ItemDesiredHeight * scaleFactor)); ;
+                        }
+                        currentRow.Clear();
+                        y += curChild.DesiredSize.Height;
+                        x = 0;
+                    }
+                    else
+                    {
+                        scaleFactor = availableSize.Width / x;
+                        foreach (var item in currentRow)
+                        {
+                            item.Measure(new Size(availableSize.Width, ItemDesiredHeight * scaleFactor)); ;
+                        }
+
+                        y += currentRow.First().DesiredSize.Height;
+                        currentRow.Clear();
+                        currentRow.Add(curChild);
+                        x = curChild.DesiredSize.Width;
+                    }
                 }
-
-                x += child.DesiredSize.Width;
-
-                maxX = Math.Max(x, maxX);
-
-                if (items.Count <= row) items.Add(new List<UIElement>());
-                items[row].Add(child);
+                else
+                {
+                    x += curChild.DesiredSize.Width;
+                    currentRow.Add(curChild);
+                }
             }
-            y = 0;
-            foreach (var rowItems in items)
-            {
-                double itemsWidth = 0;
-                foreach (var item in rowItems)
-                {
-                    itemsWidth += item.DesiredSize.Width;
-                }
-                var scaleFactor = availableSize.Width / itemsWidth;
-                foreach (var item in rowItems)
-                {
-                    item.Measure(new Size(availableSize.Width, ItemDesiredHeight * scaleFactor)); ;
-                }
-                y += rowItems[0].DesiredSize.Height;
-            }
 
-            resultSize.Width = maxX;
+            if (currentRow.Count > 0) y += currentRow.First().DesiredSize.Height;
+
+            resultSize.Width = availableSize.Width;
             resultSize.Height = y;
 
             return resultSize;
@@ -94,13 +113,30 @@ namespace Comet.Controls
 
             foreach (var child in this.Children)
             {
-                if (x + child.DesiredSize.Width > finalSize.Width)
+                if (Math.Floor(x + child.DesiredSize.Width) > finalSize.Width + 2)
                 {
                     x = 0;
                     y += previousChildHeight;
                 }
 
-                child.Arrange(new Rect(x, y, child.DesiredSize.Width, child.DesiredSize.Height));
+                //child.Arrange(new Rect(x, y, child.DesiredSize.Width, child.DesiredSize.Height));
+                child.Arrange(new Rect(0, 0, child.DesiredSize.Width, child.DesiredSize.Height));
+                var visual = ElementCompositionPreview.GetElementVisual(child);
+
+                if (firstTime)
+                {
+                    visual.Offset = new Vector3((float)x, (float)y, 0);
+                    firstTime = false;
+                    if (OrganizeCustomPanelLoaded != null) OrganizeCustomPanelLoaded(this, EventArgs.Empty);
+                }
+                else
+                {
+                    var offsetAnimation = visual.Compositor.CreateVector3KeyFrameAnimation();
+                    offsetAnimation.Duration = TimeSpan.FromMilliseconds(200);
+                    offsetAnimation.InsertKeyFrame(1, new Vector3((float)x, (float)y, 0));
+                    visual.StartAnimation("Offset", offsetAnimation);
+                }
+
 
                 x += child.DesiredSize.Width;
                 previousChildHeight = child.DesiredSize.Height;
